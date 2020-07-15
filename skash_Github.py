f6af99c15de5c2b1cc1e901ef04d1c06df73a211
92321623kh <huisrt3813@pasco.o.jp>
2020-07-15 11:58:53 +0900

    やり直し時のボールの発生位置をランダム化

--- skash_Github.py
+++ skash_Github.py
@@ -10,12 +10,43 @@ win = Tk()
 cv = Canvas(win,width = 640,height = 480)
 cv.pack()
 
+
+#ストックウィンドウ
+root = Tk()
+#ストックウィンドウ位置指定
+root.geometry('280x150+10+40')
+root.title('ストック')
+frame1 = ttk.Frame(root)
+frame1.grid()
+style = ttk.Style()
+style.theme_use('classic')
+
+
+#ラベル１表示
+label1 = ttk.Label(
+    frame1,
+    text='残り',
+    background='#0000aa',
+    foreground='#ffffff',
+    padding=(5, 10))
+label1.grid(row=0, column=0)
+
+#ラベル２表示
+label2 = ttk.Label(
+    frame1,
+    text='3',
+    background='#ffffff',
+    width=20,
+    anchor=E,
+    padding=(5, 10))
+label2.grid(row=0, column=2)
+
 #ゲームの初期化
 def init_game():
     global is_gameover,ball_ichi_x,ball_ichi_y
     global ball_idou_x,ball_idou_y,ball_size
-    global racket_ichi_x,racket_size,racket_left,racket_center,racket_right,point,speed
-    global block_ichi_x,block_size,point
+    global racket_ichi_x,racket_size,point,speed
+    global block_ichi_x,block_size,point,stock
     
     is_gameover = False
     ball_ichi_x = 0
@@ -25,17 +56,14 @@ def init_game():
     ball_size = 10
     racket_ichi_x = 0
     racket_size = 100
-    racket_left = 40 
-    racket_center = 20
-    racket_right = 40
     block_ichi_x = 100
     block_size = 100
     stock_ichi_x = 100
     stock_ichi_y = 100
     point = 10
+    stock = 3
     speed = 70
     win.title("スカッシュゲームスタート！")
-
 
 #画面描画
 def draw_screen():
@@ -52,21 +80,22 @@ def draw_ball():
     
 def draw_racket():
     #ラケットを描く
-    cv.create_rectangle(racket_ichi_x,470,racket_ichi_x +100,480,fill="yellow")
-    cv.create_rectangle(racket_ichi_x+100,470,(racket_ichi_x +100)+100,480,fill="blue")
-    cv.create_rectangle(racket_ichi_x+200,470,(racket_ichi_x +200)+100,480,fill="green")
+    cv.create_rectangle(racket_ichi_x,470,racket_ichi_x +40,480,fill="yellow")
+    cv.create_rectangle(racket_ichi_x+40,470,(racket_ichi_x +40)+40,480,fill="yellow")
+    cv.create_rectangle(racket_ichi_x+80,470,(racket_ichi_x +80)+40,480,fill="yellow")
     
     
 def draw_block():
     #障害物を描く
-    cv.create_rectangle(block_ichi_x,66,block_ichi_x +76,138,fill="blue")
+    cv.create_rectangle(block_ichi_x,106,block_ichi_x +76,138,fill="blue")
 
 #ボールの移動
+ ##グローバル関数定義
 def move_ball():
-    global is_gameover,point,ball_ichi_x,ball_ichi_y,ball_idou_x,ball_idou_y
+    global is_gameover,point,ball_ichi_x,ball_ichi_y,ball_idou_x,ball_idou_y,stock
     if is_gameover: return
 
- #左右の壁に当たったかの判定
+#左右の壁に当たったかの判定
     if ball_ichi_x + ball_idou_x < 0 or ball_ichi_x + ball_idou_x > 640:
         ball_idou_x *= -1
 
@@ -74,23 +103,23 @@ def move_ball():
     if ball_ichi_y + ball_idou_y < 0:
         ball_idou_y *= -1
         
- #ラケットに当たったか判定
+#ラケットに当たったか判定
     #ラケット左側当たり判定
     if ball_ichi_y + ball_idou_y > 470 and (
         racket_ichi_x <= (ball_ichi_x + ball_idou_x) <=
-       (racket_ichi_x + 100)
+       (racket_ichi_x + 40)
         ):
         ball_idou_y *= -1
         
-        if ball_ichi_x + ball_idou_x > (racket_ichi_x + 100):
+        if ball_idou_x > 0:
             ball_idou_x *= -1
-        if ball_ichi_x + ball_idou_x < racket_ichi_x:
+        if ball_idou_x < 0:
             ball_idou_x *= 1
 
     #ラケット中央当たり判定
     if ball_ichi_y + ball_idou_y > 470 and (
-        racket_ichi_x+100 <= (ball_ichi_x + ball_idou_x) <=
-       ((racket_ichi_x + 100)+100)
+        racket_ichi_x+40 <= (ball_ichi_x + ball_idou_x) <=
+       ((racket_ichi_x + 40)+40)
         ):
         ball_idou_y *= -1
         if random.randint(0, 1) == 0:
@@ -98,11 +127,14 @@ def move_ball():
 
     #ラケット右側当たり判定
     if ball_ichi_y + ball_idou_y > 470 and (
-        racket_ichi_x+200 <= (ball_ichi_x + ball_idou_x) <=
-       ((racket_ichi_x + 200)+100)
+        racket_ichi_x+80 <= (ball_ichi_x + ball_idou_x) <=
+       ((racket_ichi_x + 80)+40)
         ):
         ball_idou_y *= -1
-        if random.randint(0, 1) == 0:
+        
+        if ball_idou_x > 0:
+            ball_idou_x *= 1
+        if ball_idou_x < 0:
             ball_idou_x *= -1
             
         mes = random.randint(0,4)
@@ -118,9 +150,10 @@ def move_ball():
             message = "素敵"
         point += 10
         win.title(message + "得点="+ str(point))
-
+       
  #ミスしたときの判定
     if ball_ichi_y + ball_idou_y >= 480:
+       
         mes = random.randint(0,2)
         if mes == 0:
             message = "下手くそ"
@@ -129,8 +162,38 @@ def move_ball():
         if mes == 2:
             message = "は？"
         win.title(message +"得点=" + str(point))
-        is_gameover = True
+     
+        stock = stock - 1
+        
+        #ストック数減少
+        if stock == 0:
+            label2 = ttk.Label(
+            frame1,
+            text= 'GAMEOVER',
+            background='#ffffff',
+            width=20,
+            anchor=E,
+            padding=(5, 10))
+            label2.grid(row=0, column=2)
+            is_gameover = True
+        #ストックが０以外の時
+        else:
+            label2 = ttk.Label(
+            frame1,
+            text=str(stock),
+            background='#ffffff',
+            width=20,
+            anchor=E,
+            padding=(5, 10))
+            label2.grid(row=0, column=2)
+            
+        #やり直しボール移動(ランダム)
+        x = random.randint(0,640)
+        y = random.randint(0,480)
+        ball_ichi_x = (ball_idou_x * -1) + x
+        ball_ichi_y = (ball_idou_y * -1) + y
 
+    #ボールが枠内の時の移動        
     if 0 <= ball_ichi_x + ball_idou_x <= 640:
         ball_ichi_x = ball_ichi_x + ball_idou_x
     if 0 <= ball_ichi_y + ball_idou_y <= 480:
@@ -152,6 +215,15 @@ def motion(event):
 
 def click(event):
     if event.num == 1:
+        #ストック数回復
+        label2 = ttk.Label(
+        frame1,
+        text='3',
+        background='#ffffff',
+        width=20,
+        anchor=E,
+        padding=(5, 10))
+        label2.grid(row=0, column=2)
         init_game()
 
 #マウスの動きとクリックの確認
